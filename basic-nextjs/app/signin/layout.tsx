eb25f7173c56d871aee4549472ec51a72f57ad5b
Nishant840 <nishant7896545@gmail.com>
2025-12-27 09:58:13 +0530

    layouts in nextJs

---
+++ basic-nextjs/app/signin/layout.tsx
@@ -0,0 +1,12 @@
+
+
+export default function({children}:{
+    children: React.ReactNode
+}){
+    return <div>
+        <div className="border-b p-1 text-center" >
+            20% off for next three days
+        </div>
+        {children}
+    </div>
+}
