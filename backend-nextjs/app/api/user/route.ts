4833403a32631de8436ec92d270e077e7e5c04a6
Nishant840 <nishant7896545@gmail.com>
2025-12-28 13:09:57 +0530

    learning routes in nextJs

---
+++ backend-nextjs/app/api/user/route.ts
@@ -0,0 +1,17 @@
+
+
+export function GET(){
+    // database logic
+    return Response.json({
+        email:"nk@gmail.com",
+        name: "Nishant kumar"
+    })
+}
+
+export function POST(){
+    // database logic
+    return Response.json({
+        email:"nk@gmail.com",
+        name: "Nishant kumar"
+    })
+}
