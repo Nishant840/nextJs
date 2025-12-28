04636f2305519e64501978dd4390268718fc93eb
Nishant840 <nishant7896545@gmail.com>
2025-12-28 17:53:44 +0530

    learning post/get method in nextJs

--- backend-nextjs/app/api/user/route.ts
+++ backend-nextjs/app/api/user/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest } from "next/server"
 
 
 export function GET(){
@@ -8,10 +9,13 @@ export function GET(){
     })
 }
 
-export function POST(){
-    // database logic
+export async function POST(req:NextRequest){
+    // extract the body
+    const body = await req.json();
+    console.log(body);
+    // store the body in databse
+
     return Response.json({
-        email:"nk@gmail.com",
-        name: "Nishant kumar"
+        message: "You are logged in!"
     })
 }
